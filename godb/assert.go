09138994a22f3213f87c179f6bfbc9a64a3fd16b
I548435 <alex.han@sap.com>
2025-06-30 10:01:44 +0000

    chore: exercise 1

---
+++ godb/assert.go
@@ -0,0 +1,16 @@
+package godb
+
+import (
+	"fmt"
+	"strings"
+)
+
+func assert(cond bool, msg ...interface{}) {
+	if !cond {
+		strs := make([]string, len(msg))
+		for i, v := range msg {
+			strs[i] = fmt.Sprint(v)
+		}
+		panic(fmt.Sprintf("assertion failed: %s", strings.Join(strs, ";")))
+	}
+}
