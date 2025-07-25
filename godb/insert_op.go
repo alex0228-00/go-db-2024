fc0096581820e46451846c9789f6c8af33d35999
Alex <hanalex109@gmail.com>
2025-07-25 14:44:54 +0800

    chore: insert and delete op (#9)
    * chore: insert and delete
    
    * chore: enable test
    
    ---------
    
    Co-authored-by: I548435 <alex.han@sap.com>

--- godb/insert_op.go
+++ godb/insert_op.go
@@ -3,20 +3,26 @@ package godb
 import "fmt"
 
 type InsertOp struct {
-	// TODO: some code goes here
+	insertFile DBFile
+	child      Operator
 }
 
 // Construct an insert operator that inserts the records in the child Operator
 // into the specified DBFile.
 func NewInsertOp(insertFile DBFile, child Operator) *InsertOp {
-	// TODO: some code goes here
-	return nil
+	return &InsertOp{
+		insertFile: insertFile,
+		child:      child,
+	}
 }
 
 // The insert TupleDesc is a one column descriptor with an integer field named "count"
 func (i *InsertOp) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return nil
+	return &TupleDesc{
+		Fields: []FieldType{
+			{"count", "", IntType},
+		},
+	}
 }
 
 // Return an iterator function that inserts all of the tuples from the child
@@ -25,6 +31,29 @@ func (i *InsertOp) Descriptor() *TupleDesc {
 // were inserted.  Tuples should be inserted using the [DBFile.insertTuple]
 // method.
 func (iop *InsertOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("InsertOp.Iterator not implemented")
+	n := 0
+	iter, err := iop.child.Iterator(tid)
+	if err != nil {
+		return nil, fmt.Errorf("error getting child iterator: %w", err)
+	}
+	return func() (*Tuple, error) {
+		for {
+			tup, err := iter()
+			if err != nil {
+				return nil, fmt.Errorf("error iterating child: %w", err)
+			}
+			if tup == nil {
+				break // no more tuples
+			}
+			err = iop.insertFile.insertTuple(tup, tid)
+			if err != nil {
+				return nil, fmt.Errorf("error inserting tuple: %w", err)
+			}
+			n++
+		}
+		return &Tuple{
+			Desc:   *iop.Descriptor(),
+			Fields: []DBValue{IntField{int64(n)}},
+		}, nil
+	}, nil
 }
