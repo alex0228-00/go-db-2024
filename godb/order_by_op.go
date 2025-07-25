30f0255b02216aca20881dbdf9670df34782fd2e
I548435 <alex.han@sap.com>
2025-07-25 07:59:13 +0000

    chore: limit and order op

--- godb/order_by_op.go
+++ godb/order_by_op.go
@@ -2,12 +2,14 @@ package godb
 
 import (
 	"fmt"
+	"sort"
 )
 
 type OrderBy struct {
 	orderBy []Expr // OrderBy should include these two fields (used by parser)
 	child   Operator
 	// TODO: You may want to add additional fields here
+	ascending []bool
 }
 
 // Construct an order by operator. Saves the list of field, child, and ascending
@@ -16,9 +18,11 @@ type OrderBy struct {
 // ascending bitmap indicates whether the ith field in the orderByFields list
 // should be in ascending (true) or descending (false) order.
 func NewOrderBy(orderByFields []Expr, child Operator, ascending []bool) (*OrderBy, error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("NewOrderBy not implemented.") //replace me
-
+	return &OrderBy{
+		orderBy:   orderByFields,
+		child:     child,
+		ascending: ascending,
+	}, nil
 }
 
 // Return the tuple descriptor.
@@ -26,8 +30,7 @@ func NewOrderBy(orderByFields []Expr, child Operator, ascending []bool) (*OrderB
 // Note that the order by just changes the order of the child tuples, not the
 // fields that are emitted.
 func (o *OrderBy) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return &TupleDesc{} // replace me
+	return o.child.Descriptor()
 }
 
 // Return a function that iterates through the results of the child iterator in
@@ -44,5 +47,59 @@ func (o *OrderBy) Descriptor() *TupleDesc {
 // https://pkg.go.dev/sort
 func (o *OrderBy) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
 	// TODO: some code goes here
-	return nil, fmt.Errorf("OrderBy.Iterator not implemented") // replace me
+	iter, err := o.child.Iterator(tid)
+	if err != nil {
+		return nil, fmt.Errorf("error getting child iterator: %w", err)
+	}
+	var tuples []*Tuple = nil
+	return func() (*Tuple, error) {
+		if tuples == nil {
+			sorted, err := o.sort(iter)
+			if err != nil {
+				return nil, fmt.Errorf("error sorting tuples: %w", err)
+			}
+			tuples = sorted
+		}
+
+		if len(tuples) == 0 {
+			return nil, nil
+		}
+		tuple := tuples[0]
+		tuples = tuples[1:] // remove the first tuple for next call
+		return tuple, nil
+	}, nil
+}
+
+func (o *OrderBy) sort(iter func() (*Tuple, error)) ([]*Tuple, error) {
+	var ret []*Tuple
+
+	for {
+		tup, err := iter()
+		if err != nil {
+			return nil, fmt.Errorf("error iterating child: %w", err)
+		}
+		if tup == nil {
+			break // no more tuples
+		}
+		ret = append(ret, tup)
+	}
+	sort.Slice(ret, func(i, j int) bool {
+		for k := 0; k < len(o.orderBy); k++ {
+			if ret[i].Fields[k].EvalPred(ret[j].Fields[k], OpEq) {
+				continue
+			}
+			if o.ascending[k] {
+				if ret[i].Fields[k].EvalPred(ret[j].Fields[k], OpLt) {
+					return true
+				}
+			} else {
+				if ret[i].Fields[k].EvalPred(ret[j].Fields[k], OpGt) {
+					return true
+				}
+			}
+			break
+		}
+		return false
+	})
+	return ret, nil
 }
