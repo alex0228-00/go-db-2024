4214aa7d0a9efca8fecc923f0c33f1b624491763
Alex <hanalex109@gmail.com>
2025-07-25 16:02:06 +0800

    feat: limit and order by op (#11)
    * chore: limit and order op
    
    * chore: enable op
    
    ---------
    
    Co-authored-by: I548435 <alex.han@sap.com>

--- godb/limit_op.go
+++ godb/limit_op.go
@@ -1,9 +1,5 @@
 package godb
 
-import (
-"fmt"
-)
-
 type LimitOp struct {
 	// Required fields for parser
 	child     Operator
@@ -19,14 +15,27 @@ func NewLimitOp(lim Expr, child Operator) *LimitOp {
 
 // Return a TupleDescriptor for this limit.
 func (l *LimitOp) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return &TupleDesc{} // replace me
+	return l.child.Descriptor()
 }
 
 // Limit operator implementation. This function should iterate over the results
 // of the child iterator, and limit the result set to the first [lim] tuples it
 // sees (where lim is specified in the constructor).
 func (l *LimitOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("LimitOp.Iterator not implemented") // replace me
+	iter, err := l.child.Iterator(tid)
+	if err != nil {
+		return nil, err
+	}
+
+	limitTuple, _ := l.limitTups.EvalExpr(nil)
+	n := limitTuple.(IntField).Value
+
+	return func() (*Tuple, error) {
+		if n == 0 {
+			return nil, nil
+		}
+		n--
+
+		return iter()
+	}, nil
 }
