0fa81a827887c4cda1ef3c27a1b131323443e305
I548435 <alex.han@sap.com>
2025-07-25 06:53:47 +0000

    chore: project op

--- godb/project_op.go
+++ godb/project_op.go
@@ -1,13 +1,15 @@
 package godb
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Project struct {
 	selectFields []Expr // required fields for parser
 	outputNames  []string
 	child        Operator
 	// You may want to add additional fields here
-	// TODO: some code goes here
+	selected map[any]struct{}
 }
 
 // Construct a projection operator. It saves the list of selected field, child,
@@ -17,8 +19,16 @@ type Project struct {
 // distinct is for noting whether the projection reports only distinct results,
 // and child is the child operator.
 func NewProjectOp(selectFields []Expr, outputNames []string, distinct bool, child Operator) (Operator, error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("NewProjectOp not implemented.") // replace me
+	op := &Project{
+		selectFields: selectFields,
+		outputNames:  outputNames,
+		child:        child,
+	}
+	if distinct {
+		op.selected = make(map[any]struct{})
+	}
+	assert(len(selectFields) == len(outputNames), "selectFields and outputNames must be of the same length")
+	return op, nil
 }
 
 // Return a TupleDescriptor for this projection. The returned descriptor should
@@ -27,8 +37,14 @@ func NewProjectOp(selectFields []Expr, outputNames []string, distinct bool, chil
 //
 // HINT: you can use expr.GetExprType() to get the field type
 func (p *Project) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return &TupleDesc{} // replace me
+	td := &TupleDesc{}
+
+	for i, expr := range p.selectFields {
+		ft := expr.GetExprType()
+		ft.Fname = p.outputNames[i]
+		td.Fields = append(td.Fields, ft)
+	}
+	return td
 
 }
 
@@ -39,6 +55,42 @@ func (p *Project) Descriptor() *TupleDesc {
 // distinct tuples seen so far. Note that support for the distinct keyword is
 // optional as specified in the lab 2 assignment.
 func (p *Project) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("Project.Iterator not implemented") // replace me
+	iter, err := p.child.Iterator(tid)
+	if err != nil {
+		return nil, fmt.Errorf("error getting child iterator: %w", err)
+	}
+	return func() (*Tuple, error) {
+		for {
+			t, err := iter()
+			if err != nil {
+				return nil, fmt.Errorf("error iterating child: %w", err)
+			}
+			if t == nil {
+				return nil, nil // no more tuples
+			}
+
+			ret := &Tuple{
+				Desc:   *p.Descriptor(),
+				Fields: make([]DBValue, 0, len(p.selectFields)),
+			}
+
+			for _, expr := range p.selectFields {
+				val, err := expr.EvalExpr(t)
+				if err != nil {
+					return nil, fmt.Errorf("error evaluating expression: %w", err)
+				}
+				ret.Fields = append(ret.Fields, val)
+			}
+
+			if p.selected != nil {
+				// Check for distinct tuples
+				key := ret.tupleKey()
+				if _, exists := p.selected[key]; exists {
+					continue // Skip duplicate tuple
+				}
+				p.selected[key] = struct{}{} // Mark this tuple as seen
+			}
+			return ret, nil
+		}
+	}, nil
 }
